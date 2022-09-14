6ee5d6bd493d70f498298d2eff47e7c29b56c868
man7788 <jerman1841@gmail.com>
2022-09-15 05:10:36 +0800

    Add button to set grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log('yes');
+const gridButton = document.querySelector('#setGrid')
+
+gridButton.addEventListener('click', () => {
+  let area;
+  area = Number(prompt('Set grid number of square per side'));
+  let gridCal = area * area;
+  createGrid(gridCal);
+});
+
+function createGrid(squares) {
+  for (i = 0; i < squares; i++) {
+  const grid = document.createElement('div');
+  grid.classList.add('grid');      
+  grid.style.width = '10px';
+  grid.style.height = '10px';
+  grid.style.border = '1px solid black';
+  grid.style.flex = '1 0 auto';
+  container.appendChild(grid);
+}};
+
+// console.log(typeof(gridCal));
+// console.log(area);
